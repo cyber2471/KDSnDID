fc7d0f7851752414b6aee17b82a37202f0f839bd
cyberpark <pcs2471@daum.net>
2023-11-10 12:50:31 +0900

    20231110-01

--- config.bat
+++ config.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "C:\Program Files (x86)\KDSnDID\App"
-npm install & prisma db pull
+npm install
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit!!? <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<body>
+
+<button onclick="openFullscreenPopup('http://www.naver.com');">KDSnDID</button>
+
+<script>
+function openFullscreenPopup(url) {
+    // 새 창을 열고 참조를 변수에 저장합니다.
+    var popupWindow = window.open(url, 'popupWindow', 'width=1280,height=1024');
+
+    // 새 창이 로드될 때 함수를 실행합니다.
+    popupWindow.onload = function () {
+        // Fullscreen API를 사용하여 전체 화면 모드로 전환합니다.
+        if (popupWindow.document.fullscreenEnabled) {
+            popupWindow.document.documentElement.requestFullscreen();
+            alert(popupWindow.document.fullscreenElement);
+        } else {
+            alert('Fullscreen mode is not enabled in your browser.');
+        }
+    };
+}
+</script>
+
+</body>
+
